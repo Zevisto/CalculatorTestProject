4a6521364b46f99ea24f8f5df9a3c5a284c5a8d7
Zevisto <zevisto@gmail.com>
2017-07-05 21:08:33 +0200

    Rebuild main class for better integration with user input.

--- src/main/java/com/CalculatorApp/CalculatorApplication.java
+++ src/main/java/com/CalculatorApp/CalculatorApplication.java
@@ -10,10 +10,37 @@ public class CalculatorApplication {
         System.out.println("Wpisz druga liczbe :");
         int wybrano2 = reader.nextInt();
         Calculator kalkulatorTest = new Calculator(wybrano, wybrano2);
+        System.out.println("Wpisz 1 - dodawanie, 2 - odejmowanie, 3 - mnozenie, 4 - zmien liczby, 5 - zakoncz program.");
+        int userChoice = reader.nextInt();
 
-        System.out.println("Dodawanie Twoich liczb : " + wybrano + " + " + wybrano2 + " = " + kalkulatorTest.dodawanie());
-        System.out.println("Odejmowanie Twoich liczb : " + wybrano + " - " + wybrano2 + " = " + kalkulatorTest.odejmowanie());
-        System.out.println("Mnozenie Twoich liczb : " + wybrano + " * " + wybrano2 + " = " + kalkulatorTest.mnożenie());
+        while(wybrano > 0 && wybrano2 > 0) {
+                if(userChoice > 5) {
+                        System.out.println("Brak opcji dla : " + userChoice);
+                }
+                if(userChoice == 1) {
+                        System.out.println("Dodawanie Twoich liczb : " + wybrano + " + " + wybrano2 + " = " + kalkulatorTest.dodawanie());
+                }
+                if(userChoice == 2) {
+                        System.out.println("Odejmowanie Twoich liczb : " + wybrano + " - " + wybrano2 + " = " + kalkulatorTest.odejmowanie());
+                }
+                if(userChoice == 3) {
+                        System.out.println("Mnozenie Twoich liczb : " + wybrano + " * " + wybrano2 + " = " + kalkulatorTest.mnożenie());
+                }
+                if(userChoice == 4) {
+                        System.out.println("Wpisz pierwszą liczbe :");
+                        wybrano = reader.nextInt();
+                        System.out.println("Wpisz druga liczbe :");
+                        wybrano2 = reader.nextInt();
+                }
+                if(userChoice == 5) {
+                        System.out.println("Program zakonczyl dzialanie.");
+                        wybrano = 0;
+                        wybrano2 = 0;
+                } else {
+                        System.out.println("Wpisz 1 - dodawanie, 2 - odejmowanie, 3 - mnozenie, 4 - zmien liczby, 5 - zakoncz program.");
+                        userChoice = reader.nextInt();
+                }
+        }
 	}
 
 }
