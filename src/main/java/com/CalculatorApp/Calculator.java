4807df1383257f3326c40095630b5302624e2d31
Zevisto <zevisto@gmail.com>
2017-07-05 19:33:30 +0200

    Scanner added for reading user input.

--- src/main/java/com/CalculatorApp/Calculator.java
+++ src/main/java/com/CalculatorApp/Calculator.java
@@ -7,7 +7,6 @@ public class Calculator {
 
     public int firstNumber;
     public int secondNumber;
-    public String informationOutput;
 
     public Calculator(int firstNumber,int secondNumber) {
         this.firstNumber = firstNumber;
@@ -19,12 +18,6 @@ public class Calculator {
     }
 
     public int odejmowanie() {
-        if(firstNumber < secondNumber) {
-            informationOutput = "First number have to be higher than the second one. Minus values aren't " +
-                    "supported by this calculator.";
-            System.out.println(informationOutput);
-            return 0;
-        }
         return firstNumber - secondNumber;
     }
 
@@ -32,11 +25,4 @@ public class Calculator {
         return firstNumber * secondNumber;
     }
 
-    public int getFirstNumber() {
-        return firstNumber;
-    }
-
-    public int getSecondNumber() {
-        return secondNumber;
-    }
 }
--- src/main/java/com/CalculatorApp/CalculatorApplication.java
+++ src/main/java/com/CalculatorApp/CalculatorApplication.java
@@ -1,14 +1,19 @@
 package com.CalculatorApp;
-import com.CalculatorApp.Calculator;
+import java.util.*;
 
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-        Calculator kalkulatorTest = new Calculator(18, 13);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Wpisz pierwszą liczbe :");
+        int wybrano = reader.nextInt();
+        System.out.println("Wpisz druga liczbe :");
+        int wybrano2 = reader.nextInt();
+        Calculator kalkulatorTest = new Calculator(wybrano, wybrano2);
 
-        System.out.println(kalkulatorTest.dodawanie());
-        System.out.println(kalkulatorTest.odejmowanie());
-        System.out.println(kalkulatorTest.mnożenie());
+        System.out.println("Dodawanie Twoich liczb : " + wybrano + " + " + wybrano2 + " = " + kalkulatorTest.dodawanie());
+        System.out.println("Odejmowanie Twoich liczb : " + wybrano + " - " + wybrano2 + " = " + kalkulatorTest.odejmowanie());
+        System.out.println("Mnozenie Twoich liczb : " + wybrano + " * " + wybrano2 + " = " + kalkulatorTest.mnożenie());
 	}
 
 }
